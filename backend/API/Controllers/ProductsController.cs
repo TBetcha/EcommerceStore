e06ba1b809a17f0179a80abc649d72458eecd56a
Tbetcha <troy.boettger@gmail.com>
2026-05-05 02:37:04 -0400

    add some data stuff to start db and start controllers

---
+++ backend/API/Controllers/ProductsController.cs
@@ -0,0 +1,34 @@
+using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class ProductsController(StoreContext context) : ControllerBase
+
+// private readonly StoreContext _context;
+//
+// public ProductsController(StoreContext context)
+// {
+//     _context = context;
+// }
+
+{
+    [HttpGet]
+    public async Task<ActionResult<List<Product>>> GetProducts()
+    {
+        return await context.Products.ToListAsync();
+    }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<Product>> GetProduct(int id)
+    {
+        var product = await context.Products.FindAsync(id);
+        if (product == null) return NotFound();
+        return product;
+    }
+
+}
