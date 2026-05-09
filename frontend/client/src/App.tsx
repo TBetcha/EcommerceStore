7f9ec8ff3ae1ab2001384966a8be34b3c8fef41f
Tbetcha <troy.boettger@gmail.com>
2026-05-09 00:07:57 -0400

    dark mode

--- frontend/client/src/App.tsx
+++ frontend/client/src/App.tsx
@@ -1,30 +1,58 @@
 import { useEffect, useState } from "react"
 import type { Product } from "../src/app/models/product"
 import Catalog from "./features/catalog/Catalog"
-import { Container } from "@mui/material"
+import { ThemeProvider, Container, createTheme, Box, CssBaseline } from "@mui/material"
 import NavBar from "../src/app/layout/NavBar"
 
 function App() {
   const [products, setProducts] = useState<Product[]>([])
+  const [ darkMode, setDarkMode ] = useState<bool>(false)
+  const paletteType = darkMode ? 'dark' : 'light'
+  const theme = createTheme({
+    palette: {
+      mode: paletteType,
+      background: {
+        default: (paletteType === 'light')
+          ? 'radial-gradient(circle, #1e3aBa, #111B27)'
+          : 'radial-gradient(circle, #baecf9, #fof9ff)'
+      }
+    }
+  })
+
 
   useEffect(() => {
     fetch('https://localhost:5001/api/products')
       .then(response => response.json())
       .then(data => setProducts(data))
   }, [])
+  
+  const toggleTheme = () => {
+     return setDarkMode(!darkMode)
+  }
 
 
   return (
-    <>
-      <NavBar />
-      <Container maxWidth='xl' sx={{mt:14}}>
-        {/* <Box sx={{display:'flex', justifyContent:'center', gap:3,  marginY:3}}> */}
-        {/*   <Typography variant='h4'>Da Store</Typography> */}
-        {/*   <Button variant='contained' onClick={addProduct}>Add a jawn</Button> */}
-        {/* </Box> */}
-        <Catalog products={products} />
-      </Container>
-    </>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <NavBar darkMode={darkMode} toggleTheme={toggleTheme} />
+      <Box
+        sx={{
+          minHeight: '100vh',
+          background: darkMode
+            ? 'radial-gradient(circle, #1e3aBa, #111B27)'
+            : 'radial-gradient(circle, #baecf9, #fof9ff)',
+          py: 6
+        }}
+      >
+        <Container maxWidth='xl' sx={{ mt: 14 }}>
+          {/* <Box sx={{display:'flex', justifyContent:'center', gap:3,  marginY:3}}> */}
+          {/*   <Typography variant='h4'>Da Store</Typography> */}
+          {/*   <Button variant='contained' onClick={addProduct}>Add a jawn</Button> */}
+          {/* </Box> */}
+          <Catalog products={products} />
+        </Container>
+      </Box>
+    </ThemeProvider>
   )
 }
 
--- frontend/client/src/app/layout/NavBar.tsx
+++ frontend/client/src/app/layout/NavBar.tsx
@@ -1,10 +1,19 @@
-import { AppBar, Toolbar, Typography } from '@mui/material'
+import { AppBar, Toolbar, Typography, IconButton } from '@mui/material'
+import {LightMode, DarkMode} from '@mui/icons-material'
 
-export default function NavBar() {
+type Props = {
+  darkMode: boolean
+  toggleTheme: () => void
+}
+export default function NavBar({darkMode, toggleTheme}: Props) {
+  // const darkMode = true;
   return (
     <AppBar position="fixed">
       <Toolbar>
         <Typography variant="h6">Da Store</Typography>
+        <IconButton onClick={() => toggleTheme(!darkMode)}>
+          {darkMode ? <DarkMode /> : <LightMode sx={{color: 'yellow'}} />}
+        </IconButton>
       </Toolbar>
 
     </AppBar>
