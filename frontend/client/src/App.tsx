4ac1a1cada6bc0b06bbce2aeb547b7e09fca33e8
Tbetcha <troy.boettger@gmail.com>
2026-05-10 02:01:43 -0400

    add react router and scaffold other pages

--- frontend/client/src/App.tsx
+++ frontend/client/src/App.tsx
@@ -1,11 +1,9 @@
-import { useEffect, useState } from "react"
-import type { Product } from "../src/app/models/product"
-import Catalog from "./features/catalog/Catalog"
+import {  useState } from "react"
 import { ThemeProvider, Container, createTheme, Box, CssBaseline } from "@mui/material"
 import NavBar from "../src/app/layout/NavBar"
+import { Outlet } from "react-router-dom"
 
 function App() {
-  const [products, setProducts] = useState<Product[]>([])
   const [ darkMode, setDarkMode ] = useState<boolean>(false)
   const paletteType = darkMode ? 'dark' : 'light'
   const theme = createTheme({
@@ -20,11 +18,6 @@ function App() {
   })
 
 
-  useEffect(() => {
-    fetch('https://localhost:5001/api/products')
-      .then(response => response.json())
-      .then(data => setProducts(data))
-  }, [])
   
   const toggleTheme = () => {
      return setDarkMode(!darkMode)
@@ -49,7 +42,7 @@ function App() {
           {/*   <Typography variant='h4'>Da Store</Typography> */}
           {/*   <Button variant='contained' onClick={addProduct}>Add a jawn</Button> */}
           {/* </Box> */}
-          <Catalog products={products} />
+          <Outlet  />
         </Container>
       </Box>
     </ThemeProvider>
