e62f2940e37a8222382b3156b46bfc07ad4ecf2e
Tbetcha <troy.boettger@gmail.com>
2026-05-17 19:55:14 -0400

    nonsense

--- frontend/client/src/features/about/AboutPage.tsx
+++ frontend/client/src/features/about/AboutPage.tsx
@@ -1,6 +1,12 @@
+import { Container, Typography, ButtonGroup, Button } from "@mui/material";
 export default function AboutPage() {
   return (
-    <div></div>
+    <Container maxWidth='lg'>
+      <Typography variant='h3' gutterBottom>Errors for testing</Typography>
+      <ButtonGroup fullWidth>
+        <Button variant="contained">Test 400 Error</Button>
+      </ButtonGroup>
+    </Container>
   )
 }
 
