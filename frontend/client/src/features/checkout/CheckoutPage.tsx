dc3b5da05c36f90514949b83eacffa89d7613611
Tbetcha <troy.boettger@gmail.com>
2026-05-26 00:05:31 -0400

    fix bug with related to nonexistent basket

---
+++ frontend/client/src/features/checkout/CheckoutPage.tsx
@@ -0,0 +1,10 @@
+import { Typography } from "@mui/material";
+
+export default function CheckoutPage() {
+  return (
+    <Typography variant="h3">
+      Only authorized users should see this
+    </Typography>
+  )
+}
+
